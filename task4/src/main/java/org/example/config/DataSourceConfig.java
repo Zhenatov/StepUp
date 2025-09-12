bea052b941d77df4610a9b0eb0fca663a49443b8
Zhenatov <ZhenatovE@gmail.com>
2025-09-12 12:02:32 +0300

    fix for fourth task

--- task4/src/main/java/org/example/config/DataSourceConfig.java
+++ task4/src/main/java/org/example/config/DataSourceConfig.java
@@ -1,19 +1,34 @@
 package org.example.config;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DataSourceConfig {
+
+    @Value("${postgresql.driver}")
+    private String driver;
+    @Value("${postgresql.username}")
+    private String userName;
+    @Value("${postgresql.password}")
+    private String password;
+    @Value("${postgresql.pool}")
+    private int pool;
+
+
 
     @Bean
     public HikariDataSource hikariDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/Task4?currentSchema=users");
-        hikariDataSource.setUsername("postgres");
-        hikariDataSource.setPassword("postgres");
-        hikariDataSource.setMaximumPoolSize(10);
-        return hikariDataSource;
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(driver);
+        hikariConfig.setUsername(userName);
+        hikariConfig.setPassword(password);
+        hikariConfig.setMaximumPoolSize(pool);
+        return new HikariDataSource(hikariConfig);
     }
 }
